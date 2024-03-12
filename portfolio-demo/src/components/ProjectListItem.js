eb194cc84ddc616801b1a08ab7e8f64106e16a55
Rachel Katz <rachel.katz@flatironschool.com>
2024-03-12 15:42:48 -0400

    demo done

--- portfolio-demo/src/components/ProjectListItem.js
+++ portfolio-demo/src/components/ProjectListItem.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
+
 function ProjectListItem({ project }) {
 	let { name, about, image, claps, link, phase } = project;
 
 	//✅ 2b. create state for claps
+	const [cardClaps, setCardClaps] = useState(claps)
 
 	//✅ 2c. create callback to update clap state on button click
+	const handleClick = () => {
+		let newClaps = cardClaps + 1 
+		setCardClaps(newClaps)
+		
+		//setCardClaps(prevCardClaps => prevCardClaps + 1)
+	}
+
 	return (
 		<li className="card">
 			<figure className="image">
 				<img src={image} alt={name} />
 				{ /*✅ 2a. create a clap button */ }
 				{ /*✅ 2d. attach an event handler to the button */ }
-				<button className="claps">👏{claps}</button>
+				<button className="claps" onClick={handleClick}>👏{cardClaps}</button>
 			</figure>
 			<section className="details">
 				<h4>{name}</h4>
